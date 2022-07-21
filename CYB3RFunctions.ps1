33b0cc5e71864bc04b5fdcf0fd76e9e1523359d5
n0isegat3 <jan@cyber-rangers.com>
2022-07-21 16:10:57 +0200

    registry dates

--- CYB3RFunctions.ps1
+++ CYB3RFunctions.ps1
@@ -37,7 +37,7 @@ function ConvertTo-Base64 {
         [string]$String
     )
     $bytes = [System.Text.Encoding]::Unicode.GetBytes($String)
-    $encoded =[Convert]::ToBase64String($bytes)
+    $encoded = [Convert]::ToBase64String($bytes)
     return $encoded
 }
 
@@ -51,4 +51,30 @@ function ConvertFrom-Base64 {
 
     $decoded = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($Base64String))
     return $decoded
+}
+
+function Convert-HexToByteArray {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Hex
+    )
+    
+    (0..([Math]::Floor( ($hex.Length + 1) / 2) - 1)).ForEach({ [Convert]::ToByte($(if ($hex.Length -ge 2 * ($_ + 1)) { $hex.Substring($_ * 2, 2) } else { $hex.Substring($_ * 2, 1).PadRight(2, '0') }), 16) })
+}
+
+function Convert-RegDateToDate {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = 'Enter registry date value in form 73-9D-84-EC-B5-45-D8-01')]
+        [ValidateNotNullOrEmpty()]
+        [string]$RegValue
+    )
+
+    $byteArray = Convert-HexToByteArray -hex ($RegValue -replace '-', '')
+    $Int64Value = [System.BitConverter]::ToInt64($byteArray, 0)
+    $date = [DateTime]::FromFileTime($Int64Value)
+    #returns datetime in UTC!
+    return $date.ToUniversalTime()
 }
